a02d2ca384903832aca29244e2ff5f187f6f31fb
dhritiman <dhritiman@dhritimans-Mac-mini.local>
2024-11-20 18:59:41 -0800

    noice

--- pomo.py
+++ pomo.py
@@ -1,29 +1,41 @@
 import argparse
 import time
 
+
+def timer(mint):
+    for s in range(mint * 60):
+        mins, secs = divmod(s, 60)
+        time.sleep(1)
+        print(
+            f"\r{"0" + str(mins) if mins < 10 else mins}m:{"0" + str(secs) if secs < 10 else secs}s",
+            end="",
+        )
+
+
 def main():
-  parser = argparse.ArgumentParser("a timer for you little terminal")
-  parser.add_argument("study_time", help="how long do you want the study session to be")
-  parser.add_argument("break_time", help="how long do you want the breaks to be")
-  parser.add_argument("long_break", help="how long do you want the breaks to be after 4 study sessions")
-  parser.add_argument("sessions", help="how many study sessions do you want to do")
+    parser = argparse.ArgumentParser("a timer for you little terminal")
+    parser.add_argument(
+        "study_time", help="how long do you want the study session to be"
+    )
+    parser.add_argument("break_time", help="how long do you want the breaks to be")
+    parser.add_argument(
+        "long_break",
+        help="how long do you want the breaks to be after 4 study sessions",
+    )
+    parser.add_argument("sessions", help="how many study sessions do you want to do")
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  print(args.study_time, args.break_time, args.sessions)
+    for c in range(int(args.sessions)):
+        print("\n\nstudy")
+        timer(int(args.study_time))
 
-  for c in range(int(args.sessions)):
-    print("study")
-    for s in range(int(args.study_time) * 60):
-      time.sleep(1)
-      print(s)
+        print("\nbreak")
+        if c % 4 != 0:
+            timer(int(args.break_time))
+        else:
+            timer(int(args.long_break))
 
-    print("break")
-    break_time = args.break_time if int(args.sessions) % 4 != 0 else args.long_break
-    for s in range(int(break_time) * 60):
-      time.sleep(1)
-      print(s)
-    
 
 if __name__ == "__main__":
-  main()
+    main()
